f93141ca4c90fe31aa0df0042dc3f00ecc288c0a
shane ferrier <chferrier@gmail.com>
2025-08-26 22:02:14 -0600

    more build work

--- src/dp-controller.js
+++ src/dp-controller.js
@@ -163,7 +163,7 @@ class DatepickerDialog extends HTMLElement {
 
   async gettemplate() {
     console.log("getTemplate");
-    fetch("./node_modules/datepicker-web-component/dist/template.html").then((tmplt) => {
+   return fetch("./node_modules/datepicker-web-component/dist/template.html").then((tmplt) => {
       tmplt.text().then((tmplHtml) => {
         const parser = new DOMParser();
         const doc = parser.parseFromString(tmplHtml, 'text/html');
